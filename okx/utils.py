45501e4712ddd74e0a780f18462dc016df9a548f
hansanyue <hansanyue@hotmail.com>
2023-11-14 22:09:44 +0800

    clean code

--- okx/utils.py
+++ okx/utils.py
@@ -4,19 +4,19 @@ import datetime
 from . import consts as c
 
 
-def sign(message, secretKey):
-    mac = hmac.new(bytes(secretKey, encoding='utf8'), bytes(message, encoding='utf-8'), digestmod='sha256')
+def sign(message, secret_key):
+    mac = hmac.new(bytes(secret_key, encoding='utf8'), bytes(message, encoding='utf-8'), digestmod='sha256')
     d = mac.digest()
     return base64.b64encode(d)
 
 
-def pre_hash(timestamp, method, request_path, body,debug = True):
-    if debug == True:
-        print('body: ',body)
+def pre_hash(timestamp, method, request_path, body, debug=True):
+    if debug:
+        print('body: ', body)
     return str(timestamp) + str.upper(method) + request_path + body
 
 
-def get_header(api_key, sign, timestamp, passphrase, flag,debug = True):
+def get_header(api_key, sign, timestamp, passphrase, flag, debug=True):
     header = dict()
     header[c.CONTENT_TYPE] = c.APPLICATION_JSON
     header[c.OK_ACCESS_KEY] = api_key
@@ -24,25 +24,26 @@ def get_header(api_key, sign, timestamp, passphrase, flag,debug = True):
     header[c.OK_ACCESS_TIMESTAMP] = str(timestamp)
     header[c.OK_ACCESS_PASSPHRASE] = passphrase
     header['x-simulated-trading'] = flag
-    if debug == True:
-        print('header: ',header)
+    if debug:
+        print('header: ', header)
     return header
 
-def get_header_no_sign(flag,debug = True):
+
+def get_header_no_sign(flag, debug=True):
     header = dict()
     header[c.CONTENT_TYPE] = c.APPLICATION_JSON
     header['x-simulated-trading'] = flag
-    if debug == True:
-        print('header: ',header)
+    if debug:
+        print('header: ', header)
     return header
+
 
 def parse_params_to_str(params):
     url = '?'
     for key, value in params.items():
-        if(value != ''):
+        if value != '':
             url = url + str(key) + '=' + str(value) + '&'
     url = url[0:-1]
-    #print('url:',url)
     return url
 
 
