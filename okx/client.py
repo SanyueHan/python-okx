ff81dcc5d27ab2c72ab6f8b7a1f356885003ea8e
hansanyue <hansanyue@hotmail.com>
2023-11-19 17:45:35 +0800

    remove debug argument

--- okx/client.py
+++ okx/client.py
@@ -7,14 +7,13 @@ from . import consts as c, utils
 
 class Client(object):
 
-    def __init__(self, api_key='-1', api_secret_key='-1', passphrase='-1', use_server_time=False, flag='1', base_api=c.API_URL, debug='True'):
+    def __init__(self, api_key='-1', api_secret_key='-1', passphrase='-1', use_server_time=False, flag='1', base_api=c.API_URL):
         self.API_KEY = api_key
         self.API_SECRET_KEY = api_secret_key
         self.PASSPHRASE = passphrase
         self.use_server_time = use_server_time
         self.flag = flag
         self.domain = base_api
-        self.debug = debug
         self.client = httpx.Client(base_url=base_api, http2=True)
 
     def _request(self, method, request_path, params):
@@ -25,14 +24,11 @@ class Client(object):
             timestamp = self._get_timestamp()
         body = json.dumps(params) if method == c.POST else ""
         if self.API_KEY != '-1':
-            sign = utils.sign(utils.pre_hash(timestamp, method, request_path, str(body), self.debug), self.API_SECRET_KEY)
-            header = utils.get_header(self.API_KEY, sign, timestamp, self.PASSPHRASE, self.flag, self.debug)
+            sign = utils.sign(utils.pre_hash(timestamp, method, request_path, str(body)), self.API_SECRET_KEY)
+            header = utils.get_header(self.API_KEY, sign, timestamp, self.PASSPHRASE, self.flag)
         else:
-            header = utils.get_header_no_sign(self.flag, self.debug)
+            header = utils.get_header_no_sign(self.flag)
         response = None
-        if self.debug:
-            print('domain:', self.domain)
-            print('url:', request_path)
         if method == c.GET:
             response = self.client.get(request_path, headers=header)
         elif method == c.POST:
--- okx/utils.py
+++ okx/utils.py
@@ -10,13 +10,11 @@ def sign(message, secret_key):
     return base64.b64encode(d)
 
 
-def pre_hash(timestamp, method, request_path, body, debug=True):
-    if debug:
-        print('body: ', body)
+def pre_hash(timestamp, method, request_path, body):
     return str(timestamp) + str.upper(method) + request_path + body
 
 
-def get_header(api_key, sign, timestamp, passphrase, flag, debug=True):
+def get_header(api_key, sign, timestamp, passphrase, flag):
     header = dict()
     header[c.CONTENT_TYPE] = c.APPLICATION_JSON
     header[c.OK_ACCESS_KEY] = api_key
@@ -24,17 +22,13 @@ def get_header(api_key, sign, timestamp, passphrase, flag, debug=True):
     header[c.OK_ACCESS_TIMESTAMP] = str(timestamp)
     header[c.OK_ACCESS_PASSPHRASE] = passphrase
     header['x-simulated-trading'] = flag
-    if debug:
-        print('header: ', header)
     return header
 
 
-def get_header_no_sign(flag, debug=True):
+def get_header_no_sign(flag):
     header = dict()
     header[c.CONTENT_TYPE] = c.APPLICATION_JSON
     header['x-simulated-trading'] = flag
-    if debug:
-        print('header: ', header)
     return header
 
 
