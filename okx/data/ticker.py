b8fbd06ad54f56428935a937e18bbe859ec33e0d
hansanyue <hansanyue@hotmail.com>
2023-12-13 03:27:07 +0800

    update Ticker

--- okx/data/ticker.py
+++ okx/data/ticker.py
@@ -1,11 +1,15 @@
 
 
 class Ticker:
-    def __init__(self, doc):
-        self._last = float(doc['last'])  # 最新成交价
+    def __init__(self, last: float):
+        self._last = last
 
     def __repr__(self):
         return f"Ticker(last={self._last})"
+
+    @classmethod
+    def from_json(cls, data) -> 'Ticker':
+        return cls(last=float(data['last']))
 
     @property
     def last(self) -> float:
